03e53c041598f84883c4357aff4e1b8e91d3ca63
Rina <soannakatrina@gmail.com>
2023-07-24 17:01:04 +0800

    oks

--- FactoryView.java
+++ FactoryView.java
@@ -1,3 +1,13 @@
-public class FactoryView{
+import javax.swing.JFrame;
+import javax.swint.JPanel;
+
+import java.awt.JButton;
+
+public class FactoryView extends JFrame{
     
+    JButton RegularVM;
+    JButton SpecialVM;
+
+    
+
 }
