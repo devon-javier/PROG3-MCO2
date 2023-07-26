427919ef42adcac220070bb37781048332b2eb50
devon-javier <devon_jarek_javier@dlsu.edu.ph>
2023-07-26 23:18:40 +0800

    Added Transaction Object

---
+++ Transaction.java
@@ -0,0 +1,38 @@
+import java.time.*;
+
+public class Transaction {
+    private LocalDate date;
+    private Item item;
+    private Money userPayment;
+    private Money change;
+
+    public Transaction(Item item, Money userPayment, Money change)    {
+        date = LocalDate.now();
+        this.item = item;
+        this.userPayment = userPayment;
+        this.change = change;
+    }
+
+    public LocalDate getDate()  {
+        return this.date;
+    }
+
+    public Item getItem()   {
+        return this.item;
+    }
+
+    public Money getPayment()   {
+        return this.userPayment;
+    }
+
+    public Money getChange()    {
+        return this.change;
+    }
+    
+   
+
+
+
+    
+    
+}
