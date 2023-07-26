766ece6262aa224b2664f6d868175a39ef6c4881
devon-javier <devon_jarek_javier@dlsu.edu.ph>
2023-07-26 23:46:11 +0800

    Added SpecialVendingMachine class

---
+++ SpecialVendingMachine.java
@@ -0,0 +1,11 @@
+public class SpecialVendingMachine extends VendingMachine {
+
+    public SpecialVendingMachine()  {
+        super();
+    }
+
+    public void editSandwich()  {
+        
+    }
+    
+}
